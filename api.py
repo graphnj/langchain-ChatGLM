79960c02d51bc6c7c9a9f44bcf12e28acb06f525
zhujinhua <zhujinhua@cestc.cn>
2023-05-24 19:15:25 +0800

    chat接口临时采用文档知识

--- api.py
+++ api.py
@@ -353,6 +353,6 @@ def api_start(host, port):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", type=str, default="0.0.0.0")
-    parser.add_argument("--port", type=int, default=7861)
+    parser.add_argument("--port", type=int, default=8091)
     args = parser.parse_args()
     api_start(args.host, args.port)
